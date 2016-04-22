c89d62baed0658b19bed1828a2d84d5256207588
JiefzzLon <kimffy24@gmail.com>
2016-04-23 02:30:11 +0800

    增加版本不对称异常

---
+++ src/main/java/com/jiefzz/ejoker/domain/InvalidOperationException.java
@@ -0,0 +1,11 @@
+package com.jiefzz.ejoker.domain;
+
+public class InvalidOperationException extends RuntimeException {
+
+	private static final long serialVersionUID = 1094438497315950211L;
+
+	public InvalidOperationException(String string) {
+		super(string);
+	}
+	
+}
