a480cd1b44323a5050cdc3337d85fbdb05256cc7
JiefzzLon <kimffy24@gmail.com>
2016-04-23 04:49:32 +0800

    尝试实现委托

--- src/main/java/com/jiefzz/ejoker/domain/DelegateAction.java
+++ src/main/java/com/jiefzz/ejoker/domain/DelegateAction.java
@@ -1,7 +1,34 @@
 package com.jiefzz.ejoker.domain;
 
 import com.jiefzz.ejoker.eventing.IDomainEvent;
+import com.jiefzz.ejoker.infrastructure.DelegateIllegalException;
+import com.jiefzz.ejoker.infrastructure.IDelegateAction;
 
-public class DelegateAction<TAggregateRoot extends IAggregateRoot, TDomainEvent extends IDomainEvent> {
+public class DelegateAction<TAggregateRoot extends IAggregateRoot, TDomainEvent extends IDomainEvent> implements IDelegateAction {
+
+	private TAggregateRoot delegator;
+	
+	@Override
+	public void delegate() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 限定只能设置一次委托对象
+	 */
+	@SuppressWarnings("unchecked")
+	@Override
+	public IDelegateAction setDelegator(Object delegator) {
+		if (this.delegator!=null) throw new DelegateIllegalException("This delefator is already has an owner!!!");
+		TAggregateRoot client;
+		try {
+			client = (TAggregateRoot ) delegator;
+			this.delegator = client;
+		} catch ( ClassCastException cce ) {
+			throw new DelegateIllegalException("This delefator is not create by " + delegator.getClass().getName(), cce);
+		}
+		return this;
+	}
 
 }
