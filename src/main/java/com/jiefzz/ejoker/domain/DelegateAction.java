57799c69f3cf7d138a9b4c9da1828de891190ea9
JiefzzLon <kimffy24@gmail.com>
2016-04-23 03:49:08 +0800

    尝试实现java的委托接口

---
+++ src/main/java/com/jiefzz/ejoker/domain/DelegateAction.java
@@ -0,0 +1,7 @@
+package com.jiefzz.ejoker.domain;
+
+import com.jiefzz.ejoker.eventing.IDomainEvent;
+
+public class DelegateAction<TAggregateRoot extends IAggregateRoot, TDomainEvent extends IDomainEvent> {
+
+}
