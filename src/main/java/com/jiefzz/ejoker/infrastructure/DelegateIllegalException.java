8ce50efe0c9f5c156318337e8f99d21edf252f56
JiefzzLon <kimffy24@gmail.com>
2016-04-23 04:47:36 +0800

    增加一个异常类供委托抛出

---
+++ src/main/java/com/jiefzz/ejoker/infrastructure/DelegateIllegalException.java
@@ -0,0 +1,27 @@
+package com.jiefzz.ejoker.infrastructure;
+
+public class DelegateIllegalException extends RuntimeException {
+
+	private static final long serialVersionUID = 6387074865683319953L;
+
+	public DelegateIllegalException() {
+	}
+
+	public DelegateIllegalException(String message) {
+		super(message);
+	}
+
+	public DelegateIllegalException(Throwable cause) {
+		super(cause);
+	}
+
+	public DelegateIllegalException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public DelegateIllegalException(String message, Throwable cause, boolean enableSuppression,
+			boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+	}
+
+}
