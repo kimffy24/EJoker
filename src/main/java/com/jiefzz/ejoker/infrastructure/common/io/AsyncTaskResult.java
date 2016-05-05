925955938042edb94d64803db92db6c0be198da9
JiefzzLon <kimffy24@gmail.com>
2016-05-06 03:54:06 +0800

    更改taskResult类的方法

--- src/main/java/com/jiefzz/ejoker/infrastructure/common/io/AsyncTaskResult.java
+++ src/main/java/com/jiefzz/ejoker/infrastructure/common/io/AsyncTaskResult.java
@@ -1,50 +1,29 @@
 package com.jiefzz.ejoker.infrastructure.common.io;
 
-public class AsyncTaskResult<T> {
-
-	public final static AsyncTaskResult<?> Success = new AsyncTaskResult<Object>(AsyncTaskStatus.Success);
-
-	private AsyncTaskStatus status = AsyncTaskStatus.Undefined;
-	private String errorMessage = null;
+public final class AsyncTaskResult<T> extends BaseAsyncTaskResult {
 
 	private T data = null;
 
 	public AsyncTaskResult(AsyncTaskStatus status) {
-		setStatus(status);
+		super(status);
 	}
 	public AsyncTaskResult(AsyncTaskStatus status, String errorMessage) {
-		this(status);
-		setErrorMessage(errorMessage);
+		super(status, errorMessage);
 	}
 	public AsyncTaskResult(AsyncTaskStatus status, T data) {
-		this(status);
+		super(status);
 		setData(data);
 	}
 	public AsyncTaskResult(AsyncTaskStatus status, String errorMessage, T data) {
-		this(status, errorMessage);
+		super(status, errorMessage);
 		setData(data);
 	}
 
 
 	/* ================= */
-	public AsyncTaskStatus getStatus() {
-		return status;
-	}
-
-	public String getErrorMessage() {
-		return errorMessage;
-	}
 
 	public T getData() {
 		return data;
-	}
-
-	private void setStatus(AsyncTaskStatus status) {
-		this.status = status;
-	}
-
-	private void setErrorMessage(String errorMessage) {
-		this.errorMessage = errorMessage;
 	}
 
 	private void setData(T data) {
