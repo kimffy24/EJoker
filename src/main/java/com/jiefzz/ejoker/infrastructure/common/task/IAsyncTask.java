7e085026e744c716fa4d17dc50972621b9dfaeea
JiefzzLon <kimffy24@gmail.com>
2016-05-06 15:07:20 +0800

    删除一个import

--- src/main/java/com/jiefzz/ejoker/infrastructure/common/task/IAsyncTask.java
+++ src/main/java/com/jiefzz/ejoker/infrastructure/common/task/IAsyncTask.java
@@ -2,8 +2,6 @@ package com.jiefzz.ejoker.infrastructure.common.task;
 
 import java.util.concurrent.Callable;
 
-import com.jiefzz.ejoker.infrastructure.common.io.BaseAsyncTaskResult;
-
 public interface IAsyncTask<TAsyncTaskResult> extends Callable<TAsyncTaskResult>{
 
 	public static final int waitTimeoutMs = 12000;
