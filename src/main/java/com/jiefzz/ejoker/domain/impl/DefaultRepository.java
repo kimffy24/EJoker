ac0f15263c9bf18373452f6a1b0a80f565fbc477
JiefzzLon <kimffy24@gmail.com>
2016-05-10 23:59:46 +0800

    更新默认仓储类

--- src/main/java/com/jiefzz/ejoker/domain/impl/DefaultRepository.java
+++ src/main/java/com/jiefzz/ejoker/domain/impl/DefaultRepository.java
@@ -4,8 +4,8 @@ import com.jiefzz.ejoker.domain.IAggregateRoot;
 import com.jiefzz.ejoker.domain.IAggregateStorage;
 import com.jiefzz.ejoker.domain.IMemoryCache;
 import com.jiefzz.ejoker.domain.IRepository;
-import com.jiefzz.ejoker.infrastructure.UnimplementException;
 import com.jiefzz.ejoker.infrastructure.common.ArgumentNullException;
+import com.jiefzz.ejoker.infrastructure.common.UnimplementException;
 
 public class DefaultRepository implements IRepository {
 
@@ -28,9 +28,8 @@ public class DefaultRepository implements IRepository {
 			throw new ArgumentNullException("aggregateRootType");
 		if (aggregateRootId == null)
 			throw new ArgumentNullException("aggregateRootId");
-		return memoryCache.get(aggregateRootId, aggregateRootType)!=null?
-				memoryCache.get(aggregateRootId, aggregateRootType):
-					aggregateRootStorage.get(aggregateRootType, aggregateRootId.toString());
+		IAggregateRoot aggregateRoot = memoryCache.get(aggregateRootId, aggregateRootType);
+		return aggregateRoot!=null?aggregateRoot:aggregateRootStorage.get(aggregateRootType, aggregateRootId.toString());
 	}
 
 }
