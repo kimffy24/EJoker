c0cee1637856d29f37e85bf8f206628a67e92037
JiefzzLon <kimffy24@gmail.com>
2016-05-11 23:54:37 +0800

    增加注释

--- src/main/java/com/jiefzz/ejoker/z/common/context/IAssemblyAnalyzer.java
+++ src/main/java/com/jiefzz/ejoker/z/common/context/IAssemblyAnalyzer.java
@@ -7,8 +7,22 @@ import java.util.Set;
 
 public interface IAssemblyAnalyzer {
 
+	/**
+	 * return the dependence map
+	 * @return
+	 */
 	public Map<String, Map<String, Field>> getDependenceMapper();
+	
+	/**
+	 * return the initialize method map
+	 * @return
+	 */
 	public Map<String, Set<Method>> getInitializeMapper();
+	
+	/**
+	 * return @EService mapper
+	 * @return
+	 */
 	public Set<Class<?>> getEServiceMapper();
 	
 }
