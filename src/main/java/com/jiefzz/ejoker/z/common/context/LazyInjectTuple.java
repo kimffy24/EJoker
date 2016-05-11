8bba219ff942328d6a431efdd9e9e39ca7716b98
JiefzzLon <kimffy24@gmail.com>
2016-05-11 23:53:16 +0800

    注释

--- src/main/java/com/jiefzz/ejoker/z/common/context/LazyInjectTuple.java
+++ src/main/java/com/jiefzz/ejoker/z/common/context/LazyInjectTuple.java
@@ -2,6 +2,12 @@ package com.jiefzz.ejoker.z.common.context;
 
 import java.lang.reflect.Field;
 
+/**
+ * On purpose, help to tell the context,
+ * the field of instance waiting inject.
+ * @author JiefzzLon
+ *
+ */
 public class LazyInjectTuple {
 
 	public Object instance;
