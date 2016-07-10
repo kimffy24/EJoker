231b7e452d26517e2c99ca3e047b459027d3b2aa
jiefzz <kimffy24@gmail.com>
2016-07-10 17:20:42 +0800

    新增注释

--- src/main/java/com/jiefzz/ejoker/utils/JavaObjectSerializeUtil.java
+++ src/main/java/com/jiefzz/ejoker/utils/JavaObjectSerializeUtil.java
@@ -9,26 +9,36 @@ import java.io.Serializable;
 
 public class JavaObjectSerializeUtil {
 
+	/**
+	 * java自带的序列化函数<br>
+	 * Java对象转字节序列
+	 * @param object
+	 * @return
+	 */
 	public final static String serialize(Serializable object){
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		try {
 			ObjectOutputStream oos = new ObjectOutputStream(baos);
 			oos.writeObject(object);
 		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
+			throw new RuntimeException(object.getClass().getName()+" serialize faild!!!", e);
 		}
 		return Base64Utils.encode(baos.toByteArray());
 	}
 	
+	/**
+	 * java自带的反序列化函数<br>
+	 * 字节序列转Java对象
+	 * @param encodeString
+	 * @return
+	 */
 	public final static Serializable deserialize(String encodeString) {
 		ObjectInputStream ois;
 		try {
 			ois = new ObjectInputStream(new ByteArrayInputStream(Base64Utils.decode(encodeString)));
 			return (Serializable )ois.readObject();
 		} catch ( Exception e ) {
-			e.printStackTrace();
-			return null;
+			throw new RuntimeException("Deserialize faild!!!", e);
 		}
 	}
 }
