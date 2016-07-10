f92b407501f4583bc45f146c9f087744ba103a88
jiefzz <kimffy24@gmail.com>
2016-07-10 16:49:52 +0800

    调整基础解析类

--- src/main/java/com/jiefzz/ejoker/z/common/context/impl/RootAssemblyAnalyzer.java
+++ src/main/java/com/jiefzz/ejoker/z/common/context/impl/RootAssemblyAnalyzer.java
@@ -1,9 +1,9 @@
+
 package com.jiefzz.ejoker.z.common.context.impl;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
@@ -28,43 +28,43 @@ public class RootAssemblyAnalyzer {
 	/**
 	 * 记录已经经过Assembly的类
 	 */
-	public final List<Class<?>> hasBeenAnalyzeClass = new ArrayList<Class<?>>();
-	
+	public final Set<Class<?>> hasBeenAnalyzeClass = new HashSet<Class<?>>();
+
 	/**
 	 * 扫描到所有的EService类
 	 */
-	public final List<Class<?>> eServiceClass = new ArrayList<Class<?>>();
-	
+	public final Set<Class<?>> eServiceClass = new HashSet<Class<?>>();
+
 	/**
 	 * 记录EService类的所有父类和继承接口。
 	 */
 	public final Map<Class<?>, Set<Class<?>>> eServiceHierarchyType = new HashMap<Class<?>, Set<Class<?>>>();
-	
+
 	/**
 	 * 接口类型/抽象类型/实现类型与EService实现类映射对象
 	 */
 	public final Map<Class<?>, Class<?>> eServiceImplementationMapper = new HashMap<Class<?>, Class<?>>();
-	
+
 	/**
 	 * EService类里面有等待被注入的属性
 	 */
 	public final Map<Class<?>, Set<Field>> eDependenceMapper = new HashMap<Class<?>, Set<Field>>();
-	
+
 	/**
 	 * EService类里面有声明要初始化执行的方法
 	 */
 	public final Map<Class<?>, Set<Method>> eInitializeMapper = new HashMap<Class<?>, Set<Method>>();
-	
+
 	/**
 	 * 带ID的EService映射
 	 */
 	public final Map<String, Class<?>> eServiceIdMapper = new HashMap<String, Class<?>>();
-	
+
 	/**
 	 * 放入扫描过的包得路径的字符串
 	 */
 	public final Set<String> hasScanPackage = new HashSet<String>();
-	
+
 	/**
 	 * 存入冲突的类型。
 	 */
@@ -74,11 +74,12 @@ public class RootAssemblyAnalyzer {
 		if ( specificPackage.lastIndexOf('.') == (specificPackage.length()-1) )
 			specificPackage = specificPackage.substring(0, specificPackage.length()-1);
 		for ( String key : hasScanPackage )
-			if(specificPackage.startsWith(key)) return;  // 传入的包是某个已经被分析的包的子包或就是已存在的包，则不再分析
+			if(specificPackage.startsWith(key)) return;// 传入的包是某个已经被分析的包的子包或就是已存在的包，则不再分析
 
 		List<Class<?>> clazzInSpecificPackage;
 		try {
 			clazzInSpecificPackage = ClassNamesScanner.scanClass(specificPackage);
+			hasScanPackage.add(specificPackage);
 		} catch (Exception e) {
 			throw new ContextRuntimeException(String.format("Exception occur whild scanning package [%s]!!!", specificPackage), e);
 		}
@@ -91,7 +92,7 @@ public class RootAssemblyAnalyzer {
 			analyzeContextAnnotation(clazz);
 		}
 	}
-	
+
 	/**
 	 * DONE: 记录是否已经被分析
 	 * DONE: 记录所有EService
@@ -101,50 +102,76 @@ public class RootAssemblyAnalyzer {
 	 * @param claxx
 	 */
 	private void analyzeContextAnnotation(final Class<?> claxx) {
-		
+
 		// 已经分析过的类就跳过
 		if(hasBeenAnalyzeClass.contains(claxx)) return;
 		else hasBeenAnalyzeClass.add(claxx);
-		
+
 		boolean isEService = claxx.isAnnotationPresent(EService.class);
+		if(!isEService) return;
+
+		// 同名方法在在不同的对象中的反射Method是不一样的，用方法名作唯一控制会更好
+		Map<String, Object> conflictMethodNames = new HashMap<String, Object>();
+		// 同名属性在在不同的对象中的反射Field是不一样的，用属性名作唯一控制会更好
+		Map<String, Object> conflictFieldNames = new HashMap<String, Object>();
 
 		// collect the method which annotate by @Initialize .
-		Set<Method> annotationMethodName = new HashSet<Method>();
+		Set<Method> annotationMethods = new HashSet<Method>();
 		// collect the properties which annotate by @Dependence or @Resource .
-		Set<Field> annotationFieldName = new HashSet<Field>();
+		Set<Field> annotationFields = new HashSet<Field>();
 		// collect the superClass or superInterface
 		Set<Class<?>> resolvedHierarchyType = new HashSet<Class<?>>();
+
 		for ( Class<?> clazz = claxx; clazz != Object.class; clazz = clazz.getSuperclass() ) {
+
+			// 扫描所有被标注为初始化的方法
 			Method[] methods = clazz.getDeclaredMethods();
 			for ( Method method : methods ) {
-				if ( annotationMethodName.contains(method) ) continue;
-				if ( method.isAnnotationPresent(Initialize.class) )
-					annotationMethodName.add(method);
+				if ( conflictMethodNames.containsKey(method.getName()) ) continue;
+				if ( method.isAnnotationPresent(Initialize.class) ) {
+					annotationMethods.add(method);
+					conflictMethodNames.put(method.getName(), method);
+				}
 			}
+
+			// 扫描所有标记为依赖的属性
 			Field[] fieldArray = clazz.getDeclaredFields();
 			for ( Field field : fieldArray ) {
 				if ( field.isAnnotationPresent(Dependence.class) || field.isAnnotationPresent(Resource.class) )
-					annotationFieldName.add(field);
+					if(!conflictFieldNames.containsKey(field.getName())) { 
+						annotationFields.add(field);
+						conflictFieldNames.put(field.getName(), field);
+					}
 			}
-			
+
 			// 收集接口映射 收集父类映射 
 			Class<?>[] interfaces = clazz.getInterfaces();
 			for ( Class<?> interfaceType : interfaces )
-				resolvedHierarchyType.add(interfaceType);
+				if(!resolvedHierarchyType.contains(interfaceType))
+					resolvedHierarchyType.add(interfaceType);
+
 			resolvedHierarchyType.add(clazz);
 		}
-		eDependenceMapper.put(claxx, annotationFieldName);
-		eInitializeMapper.put(claxx, annotationMethodName);
-
-		if(isEService) {
+		eDependenceMapper.put(claxx, annotationFields);
+		eInitializeMapper.put(claxx, annotationMethods);
 		eServiceHierarchyType.put(claxx, resolvedHierarchyType);
-		for(Class<?> hierarchyType : resolvedHierarchyType)
-			if(null!=eServiceImplementationMapper.putIfAbsent(hierarchyType, claxx)) {
-				eServiceImplementationMapper.remove(hierarchyType);
-				conflictResolvType.add(hierarchyType);
+
+		for(Class<?> hierarchyType : resolvedHierarchyType) {
+			if(conflictResolvType.contains(hierarchyType)) {
 				logger.warn("[{}] has more than one @EService implementation!", hierarchyType.getName());
+				logger.warn("[{}] can not be resolve to [{}]", hierarchyType.getName(), claxx.getName());
+				continue;
+			}
+			Class<?> previousResolvedType=eServiceImplementationMapper.putIfAbsent(hierarchyType, claxx);
+			if(null!=previousResolvedType) {
+				conflictResolvType.add(hierarchyType);
+				eServiceImplementationMapper.remove(hierarchyType);
+				logger.warn("[{}] has more than one @EService implementation!", hierarchyType.getName());
+				logger.warn("[{}] can not be resolve to [{}]", hierarchyType.getName(), previousResolvedType.getName());
+				logger.warn("[{}] has more than one @EService implementation!", hierarchyType.getName());
+				logger.warn("[{}] can not be resolve to [{}]", hierarchyType.getName(), claxx.getName());
 			}
 		}
 	}
-	
+
 }
