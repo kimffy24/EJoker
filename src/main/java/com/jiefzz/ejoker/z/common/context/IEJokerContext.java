9d48cf51f4a8aebc63b221e72e7b3ab77afdb51d
JiefzzLon <kimffy24@gmail.com>
2016-10-29 01:45:25 +0800

    清理旧的实现

--- src/main/java/com/jiefzz/ejoker/z/common/context/IEJokerContext.java
+++ src/main/java/com/jiefzz/ejoker/z/common/context/IEJokerContext.java
@@ -4,24 +4,17 @@ public interface IEJokerContext extends IEJokerSimpleContext {
 	
 	/**
 	 * 注册实例
+	 * @deprecated 暂时未完成
 	 * @param instance
 	 * @param clazz
 	 */
 	public <T> void regist(Object instance, Class<T> clazz);
 	/**
 	 * 带泛型签名注册实例
+	 * @deprecated 暂时未完成
 	 * @param instance
 	 * @param clazz
 	 * @param pSign
 	 */
 	public <T> void regist(Object instance, Class<T> clazz, String pSignature);
-	
-	/**
-	 * 带识别符注册实例
-	 * @param instance
-	 * @param clazz
-	 * @param pSign
-	 */
-	public <T> void regist(Object instance, String instanceId);
-	
 }
