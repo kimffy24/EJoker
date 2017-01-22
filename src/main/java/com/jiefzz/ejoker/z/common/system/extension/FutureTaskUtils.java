4ce7f2bb1778aabf9d777faf80af880f0ebd1c13
JiefzzLon <kimffy24@gmail.com>
2017-01-23 03:43:10 +0800

    优化结构

--- src/main/java/com/jiefzz/ejoker/z/common/system/extension/FutureTaskUtils.java
+++ src/main/java/com/jiefzz/ejoker/z/common/system/extension/FutureTaskUtils.java
@@ -12,16 +12,11 @@ import java.util.concurrent.FutureTask;
  */
 public class FutureTaskUtils {
 	
-	private final static Method method;
-	static {
-		try {
-			method = FutureTask.class.getDeclaredMethod("set", Object.class);
-			method.setAccessible(true);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
-
+	/**
+	 * 从给定结果中构建FutureTask对象
+	 * @param result
+	 * @return
+	 */
 	public final static <TResult> FutureTask<TResult> buildFromResult(TResult result) {
 		
 		FutureTask<TResult> future = new FutureTask<TResult>(new Runnable(){
@@ -34,6 +29,16 @@ public class FutureTaskUtils {
 			return future;
 		} catch (Exception e) {
 			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+	}
+	
+	private final static Method method;
+	static {
+		try {
+			method = FutureTask.class.getDeclaredMethod("set", Object.class);
+			method.setAccessible(true);
+		} catch (Exception e) {
 			throw new RuntimeException(e);
 		}
 	}
