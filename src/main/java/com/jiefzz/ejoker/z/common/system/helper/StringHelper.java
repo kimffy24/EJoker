125ab92917855823d9ca58911d47f50feb267b2b
JiefzzLon <kimffy24@gmail.com>
2017-01-23 03:40:36 +0800

    优化结构

--- src/main/java/com/jiefzz/ejoker/z/common/system/helper/StringHelper.java
+++ src/main/java/com/jiefzz/ejoker/z/common/system/helper/StringHelper.java
@@ -3,8 +3,6 @@ package com.jiefzz.ejoker.z.common.system.helper;
 public class StringHelper {
 
 	public static boolean isNullOrEmpty(String targetString) {
-		if(null==targetString) return true;
-		if("".equals(targetString)) return true;
-		return false;
+		return (null==targetString || "".equals(targetString))? true:false;
 	}
 }
