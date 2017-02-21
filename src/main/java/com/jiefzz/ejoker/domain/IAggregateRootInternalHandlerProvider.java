73ef209cf482a862fe9026bbf995ec22d4c4beb6
JiefzzLon <kimffy24@gmail.com>
2017-02-21 16:55:39 +0800

    清理一些类似委托的实现，java没原生支持的委托

--- src/main/java/com/jiefzz/ejoker/domain/IAggregateRootInternalHandlerProvider.java
+++ src/main/java/com/jiefzz/ejoker/domain/IAggregateRootInternalHandlerProvider.java
@@ -1,8 +1,5 @@
 package com.jiefzz.ejoker.domain;
 
-import com.jiefzz.ejoker.eventing.IDomainEvent;
-import com.jiefzz.ejoker.utils.IDelegateAction;
-
 /**
  * @deprecated this class is use in eNode, but not use in eJoker
  * @author jiefzzLon
@@ -11,6 +8,5 @@ import com.jiefzz.ejoker.utils.IDelegateAction;
 public interface IAggregateRootInternalHandlerProvider {
 
 	//public DelegateAction<IAggregateRoot, IDomainEvent> getInternalEventHandler(Class<? extends IAggregateRoot> aggregateRootType, Class<? extends IDomainEvent> eventType);
-	
-	public <TAggregateRoot extends IAggregateRoot, TDomainEvent extends IDomainEvent<?>> IDelegateAction<TAggregateRoot, TDomainEvent> getInnerEventHandler(Class<TAggregateRoot> aggregateRootType, Class<TDomainEvent> eventType);
+
 }
--- src/main/java/com/jiefzz/ejoker/utils/IDelegateAction.java
+++
@@ -1,7 +0,0 @@
-package com.jiefzz.ejoker.utils;
-
-public interface IDelegateAction<TDelegator, TParameter> {
-
-	public void delegate(TDelegator delegator, TParameter parameter);
-
-}
