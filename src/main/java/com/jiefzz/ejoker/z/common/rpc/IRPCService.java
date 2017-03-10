67fda29c051e004899c906024bba60e6b59e27bf
JiefzzLon <kimffy24@gmail.com>
2017-03-11 01:27:28 +0800

    引入netty做RPC基础

--- src/main/java/com/jiefzz/ejoker/z/common/rpc/IRPCService.java
+++ src/main/java/com/jiefzz/ejoker/z/common/rpc/IRPCService.java
@@ -2,6 +2,8 @@ package com.jiefzz.ejoker.z.common.rpc;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 import com.jiefzz.ejoker.z.common.action.Action;
 
@@ -11,9 +13,21 @@ import com.jiefzz.ejoker.z.common.action.Action;
  */
 public interface IRPCService<TType> {
 	
-	final static Map<Integer, Action<?>> portMap = new HashMap<Integer, Action<?>>();
+	final static Map<Integer, RPCTuple> portMap = new HashMap<Integer, RPCTuple>();
+	
+	final static Lock rpcRegistLock = new ReentrantLock();
 
 	public void export(Action<TType> action, final int port);
 	
 	public void remoteInvoke(TType data, final String host, final int port);
+
+	public static class RPCTuple {
+		public final Thread ioThread;
+		public final Action action;
+		public RPCTuple(Action action, Thread ioThread) {
+			this.ioThread = ioThread;
+			this.action = action;
+		}
+		
+	}
 }
