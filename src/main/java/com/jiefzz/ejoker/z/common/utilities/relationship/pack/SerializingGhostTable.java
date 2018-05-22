4940cba3c7ed110160dc91f62218592e74530361
JiefzzLon <kimffy24@gmail.com>
2018-05-22 22:55:06 +0800

    创建多路装配器

---
+++ src/main/java/com/jiefzz/ejoker/z/common/utilities/relationship/pack/SerializingGhostTable.java
@@ -0,0 +1,13 @@
+package com.jiefzz.ejoker.z.common.utilities.relationship.pack;
+
+public final class SerializingGhostTable {
+
+	public final static String MAP = "M";
+
+	public final static String NORMAL = "N";
+
+	public final static String LIST = "L";
+
+	public final static String SET = "S";
+	
+}
