5209d24bfb6fc449aa63fccdb6dd8f6ed8e2bef9
JiefzzLon <kimffy24@gmail.com>
2018-09-20 19:48:35 +0800

    继承一些构造函数

--- src/main/java/com/jiefzz/ejoker/queue/completation/DefaultMQProducer.java
+++ src/main/java/com/jiefzz/ejoker/queue/completation/DefaultMQProducer.java
@@ -1,8 +1,26 @@
 package com.jiefzz.ejoker.queue.completation;
 
+import org.apache.rocketmq.remoting.RPCHook;
+
 import com.jiefzz.ejoker.z.common.context.annotation.context.EService;
 
 @EService
 public class DefaultMQProducer extends org.apache.rocketmq.client.producer.DefaultMQProducer {
+
+	public DefaultMQProducer() {
+		super();
+	}
+
+	public DefaultMQProducer(RPCHook rpcHook) {
+		super(rpcHook);
+	}
+
+	public DefaultMQProducer(String producerGroup, RPCHook rpcHook) {
+		super(producerGroup, rpcHook);
+	}
+
+	public DefaultMQProducer(String producerGroup) {
+		super(producerGroup);
+	}
 
 }
