c94e26dc7d69aa90c09f473e133e1a0b22bbb6c6
JiefzzLon <kimffy24@gmail.com>
2018-10-14 17:53:13 +0800

    添加默认实现

---
+++ src/main/java/com/jiefzz/ejokerDefaultImpl/DefaultEventSerializer.java
@@ -0,0 +1,8 @@
+package com.jiefzz.ejokerDefaultImpl;
+
+import com.jiefzz.ejoker.z.common.context.annotation.context.EService;
+
+@EService
+public class DefaultEventSerializer extends com.jiefzz.ejoker.eventing.impl.DefaultEventSerializer {
+
+}
