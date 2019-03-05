f3dcf6d777e9278d720ff55d2edbc42b78e7089d
JiefzzLon <kimffy24@gmail.com>
2019-03-05 14:45:34 +0800

    优化

--- src/main/java/com/jiefzz/ejoker/queue/completation/DefaultMQConsumer.java
+++ src/main/java/com/jiefzz/ejoker/queue/completation/DefaultMQConsumer.java
@@ -39,50 +39,39 @@ public class DefaultMQConsumer extends org.apache.rocketmq.client.consumer.Defau
 	
 	private final static Logger logger = LoggerFactory.getLogger(DefaultMQConsumer.class);
 	
-	private final static AtomicInteger processComsumedSequenceThreadCounter = new AtomicInteger(0);
-
-	private final static AtomicInteger dashboardWorkThreadCounter = new AtomicInteger(0);
-
-	private String focusTopic = "";
+	
+	private final AtomicInteger dashboardWorkThreadCounter = new AtomicInteger(0);
 	
 	private final AtomicBoolean onRunning = new AtomicBoolean(false);
 
 	private final AtomicBoolean onPasue = new AtomicBoolean(true);
 	
-	private final IVoidFunction3<Throwable, MessageQueue, ControlStruct> exHandler = (e, mq, d) -> {
-		logger.error(
-				String.format("Some exception occur!!! dashboard.offsetConsumedLocal: %d, dashboard.offsetFetchLocal: %d, mq: %s",
-					d.offsetConsumedLocal.get(),
-					d.offsetFetchLocal.get(),
-					mq.toString()),
-				e);
-	};
+	private final AtomicInteger consumingAmount = new AtomicInteger(0);
+
+	private final Set<MessageQueue> matchQueue = new HashSet<>();
+	
+	private final Map<MessageQueue, ControlStruct> dashboards = new HashMap<>();
+
+	private String focusTopic = "";
 
 	private IFunction1<Boolean, MessageQueue> queueMatcher = null;
 
-	private Set<MessageQueue> matchQueue = new HashSet<>();
-
 	private IVoidFunction2<EJokerQueueMessage, IEJokerQueueMessageContext> messageProcessor = null;
-	
-	private Map<MessageQueue, ControlStruct> dashboards = new HashMap<>();
 
-	private final Thread processComsumedSequenceThread = new Thread(() -> {
-		while (null == dashboards) {
-			try {
-				TimeUnit.SECONDS.sleep(1l);
-			} catch (InterruptedException e) { }
-		}
-		while (onRunning.get()) {
-			ForEachHelper.processForEach(dashboards, (q, d) -> processComsumedSequence(d));
-			LockSupport.park();
-		}
-	}, "processComsumedSequenceThread-" + processComsumedSequenceThreadCounter.getAndIncrement());
+	private Thread processComsumedSequenceThread = null;
+	
+	private IVoidFunction3<Throwable, MessageQueue, ControlStruct> exHandler = null;
+
+
+	
+	/**
+	 * 默认异步策略
+	 */
+	private IVoidFunction1<IVoidFunction> sumbiter = c -> new Thread(c::trigger).start();
+	
+	
 	
 	private boolean flowControlFlag = false;
-	
-	private final AtomicBoolean flowControlLoggerAccquired = new AtomicBoolean(false);
-	
-	private final AtomicInteger consumingAmount = new AtomicInteger(0);
 	
 	/**
 	 * 1 返回值boolean是否要求流控 true:是 false:不是
@@ -92,20 +81,28 @@ public class DefaultMQConsumer extends org.apache.rocketmq.client.consumer.Defau
 	 */
 	private IFunction3<Boolean, MessageQueue, Integer, Integer> flowControlSwitch = (mq, consumingAmount, n) -> false;
 	
+	private final AtomicBoolean flowControlLoggerAccquired = new AtomicBoolean(false);
+	
+	
+	
 	public DefaultMQConsumer() {
 		super();
+		postInit();
 	}
 
 	public DefaultMQConsumer(RPCHook rpcHook) {
 		super(rpcHook);
+		postInit();
 	}
 
 	public DefaultMQConsumer(String consumerGroup, RPCHook rpcHook) {
 		super(consumerGroup, rpcHook);
+		postInit();
 	}
 
 	public DefaultMQConsumer(String consumerGroup) {
 		super(consumerGroup);
+		postInit();
 	}
 	
 	public void registerEJokerCallback(IVoidFunction2<EJokerQueueMessage, IEJokerQueueMessageContext> vf) {
@@ -121,8 +118,12 @@ public class DefaultMQConsumer extends org.apache.rocketmq.client.consumer.Defau
 	public void start() throws MQClientException {
 		super.start();
 		
-		onRunning.compareAndSet(false, true);
+		if(!onRunning.compareAndSet(false, true))
+			throw new RuntimeException("Consumer has been started before!!!");
+		
 		loadSubcribeInfoAndPrepareConsume();
+		
+		processComsumedSequenceThread.start();
 
 		ForEachHelper.processForEach(dashboards, (mq, dashboard) -> {
 			dashboard.workThread = new Thread(() -> {
@@ -151,30 +152,15 @@ public class DefaultMQConsumer extends org.apache.rocketmq.client.consumer.Defau
 					} finally {
 						dashboard.isWorking.unlock();
 					}
-			}, "dashboardWorkThread-" + dashboardWorkThreadCounter.getAndIncrement());
+			}, this.getConsumerGroup() + "-dashboardWorkThread-" + dashboardWorkThreadCounter.getAndIncrement());
 			dashboard.workThread.setDaemon(true);
 			dashboard.workThread.start(); 
 		});
-		
-		processComsumedSequenceThread.setDaemon(true);
-		processComsumedSequenceThread.start();
 		
 		sumbiter.trigger(() -> {
 			SleepWrapper.sleep(TimeUnit.MILLISECONDS, 600l);
 			onPasue.set(false);
 		});
-		
-		// debug, provides a permit per 2 seconds.
-		Thread monitorThread = new Thread(() -> {
-			while(true) {
-				try {
-					flowControlLoggerAccquired.compareAndSet(false, true);
-					SleepWrapper.sleep(TimeUnit.SECONDS, 2l);
-				} catch (Exception e) {}
-			}
-		}, "monitorThread");
-		monitorThread.setDaemon(true);
-		monitorThread.start();
 	}
 	
 	public void shutdown() {
@@ -203,112 +189,6 @@ public class DefaultMQConsumer extends org.apache.rocketmq.client.consumer.Defau
 		super.shutdown();
 	}
 	
-	private void loadSubcribeInfoAndPrepareConsume() {
-
-		Set<MessageQueue> messageQueues;
-		try {
-			messageQueues = fetchSubscribeMessageQueues(focusTopic);
-		} catch (MQClientException e) {
-			e.printStackTrace();
-			throw new RuntimeException(e);
-		}
-
-		for (final MessageQueue mq : messageQueues) {
-
-			if (null != queueMatcher && !queueMatcher.trigger(mq)) {
-				continue;
-			}
-
-			final AtomicLong maxOffset;
-			long consumedOffset;
-			try {
-				maxOffset = new AtomicLong(maxOffset(mq));
-				consumedOffset = fetchConsumeOffset(mq, false);
-			} catch (MQClientException e3) {
-				throw new RuntimeException(e3);
-			}
-
-			matchQueue.add(mq);
-			dashboards.put(mq, new ControlStruct(
-					consumedOffset,
-					() -> {
-						
-						/// 语法上无法从lambda中获取到内部类的成员变量或内部类的this指针
-						/// 只能从运行时中获取
-						ControlStruct controlStruct = dashboards.get(mq);
-						
-						long currentOffset = controlStruct.offsetFetchLocal.get();
-						
-						// TODO tag 置为 null，消费端让mqSelecter发挥作用，tag让其在生产端发挥作用吧
-						PullResult pullResult;
-						try {
-							pullResult = pullBlockIfNotFound(mq, null, currentOffset, 32);
-						} catch (MQClientException | RemotingException | MQBrokerException | InterruptedException e) {
-							throw new RuntimeException(e);
-						}
-								
-						switch (pullResult.getPullStatus()) {
-							case FOUND:
-								List<MessageExt> messageExtList = pullResult.getMsgFoundList();
-								for (int i = 0; i<messageExtList.size(); i++) {
-									if(DefaultMQConsumer.this.flowControlFlag) {
-										// 流控,
-										int frezon = 0;
-										int cAmount = 0;
-										while(flowControlSwitch.trigger(mq, cAmount = consumingAmount.get(), frezon)) {
-											if(flowControlLoggerAccquired.compareAndSet(true, false))
-												logger.error("Flow control protected! Amount of on processing message is {}", cAmount);
-											LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(100l));
-											frezon++;
-										}
-										consumingAmount.getAndIncrement();
-									}
-									final long consumingOffset = currentOffset + i + 1;
-									MessageExt rmqMsg = messageExtList.get(i);
-									final EJokerQueueMessage queueMessage = new EJokerQueueMessage(
-										rmqMsg.getTopic(),
-										rmqMsg.getFlag(),
-										rmqMsg.getBody(),
-										rmqMsg.getTags());
-									sumbiter.trigger(() -> 
-									messageProcessor.trigger(queueMessage, message -> tryMarkCompletion(mq, consumingOffset))
-									)
-									;
-								}
-								controlStruct.offsetFetchLocal.getAndSet(pullResult.getNextBeginOffset());
-								maxOffset.getAndSet(pullResult.getMaxOffset());
-								break;
-							case NO_MATCHED_MSG:
-								logger.debug("[state: NO_MATCHED_MSG, topic: {}, queueId: {}]", mq.getTopic(), mq.getQueueId());
-								break;
-							case NO_NEW_MSG:
-								logger.debug("[state: NO_NEW_MSG, topic: {}, queueId: {}]", mq.getTopic(), mq.getQueueId());
-								break;
-							case OFFSET_ILLEGAL:
-								logger.debug("[state: OFFSET_ILLEGAL, topic: {}, queueId: {}]", mq.getTopic(), mq.getQueueId());
-								throw new RuntimeException("OFFSET_ILLEGAL");
-							default:
-								assert false;
-						}
-					}
-				)
-			);
-		}
-		
-		if(0 == matchQueue.size())
-			throw new RuntimeException("No queue was selected!!!");
-		
-	}
-	
-	private void tryMarkCompletion(MessageQueue mq, long comsumedOffset) {
-		ControlStruct controlStruct = dashboards.get(mq);
-		if(null != controlStruct.aheadCompletion.putIfAbsent(comsumedOffset, ""))
-			throw new RuntimeException();
-		logger.debug("Receive local completion. Queue: {}, offset {}", mq, comsumedOffset);
-		
-		enableSequenceProcessing();
-	}
-	
 	public void syncOffsetToBroker() {
 		for(MessageQueue mq : matchQueue) {
 			ControlStruct controlStruct = dashboards.get(mq);
@@ -329,6 +209,165 @@ public class DefaultMQConsumer extends org.apache.rocketmq.client.consumer.Defau
 		}
 		getOffsetStore().persistAll(matchQueue);
 		
+	}
+	
+	public void useSubmiter(IVoidFunction1<IVoidFunction> sumbiter) {
+		this.sumbiter = sumbiter;
+	}
+	
+	public void configureFlowControl(boolean flag) {
+		this.flowControlFlag = flag;
+	}
+
+	/**
+	 * 1 返回值boolean是否要求流控 true:是 false:不是
+	 * 参数1 当前队列
+	 * 参数2 在处理中消息数
+	 * 参数3 队列检查序数
+	 */
+	public void useFlowControlSwitch(IFunction3<Boolean, MessageQueue, Integer, Integer> sw) {
+		this.flowControlSwitch = sw;
+	}
+	
+	public void useQueueSelector(IFunction1<Boolean, MessageQueue> queueMatcher) {
+		this.queueMatcher = queueMatcher;
+	}
+	
+	private void postInit() {
+		processComsumedSequenceThread = new Thread(() -> {
+			while (null == dashboards) {
+				try {
+					TimeUnit.SECONDS.sleep(1l);
+				} catch (InterruptedException e) { }
+			}
+			while (onRunning.get()) {
+				ForEachHelper.processForEach(dashboards, (q, d) -> processComsumedSequence(d));
+				// 流控日志打印的许可检查，
+				// 每完成一个获得1个许可，同一时间最多1个许可，多于1个无效
+				flowControlLoggerAccquired.compareAndSet(false, true);
+				LockSupport.park();
+			}
+		}, "processComsumedSequenceThread-" + this.getConsumerGroup());
+		processComsumedSequenceThread.setDaemon(true);
+		
+		exHandler = (e, mq, d) -> logger.error(
+					String.format("Some exception occur!!! dashboard.offsetConsumedLocal: %d, dashboard.offsetFetchLocal: %d, mq: %s",
+						d.offsetConsumedLocal.get(),
+						d.offsetFetchLocal.get(),
+						mq.toString()),
+					e);
+	}
+	
+	private void loadSubcribeInfoAndPrepareConsume() {
+
+		Set<MessageQueue> messageQueues;
+		try {
+			messageQueues = fetchSubscribeMessageQueues(focusTopic);
+		} catch (MQClientException e) {
+			e.printStackTrace();
+			throw new RuntimeException(e);
+		}
+		
+		matchQueue.clear();
+		dashboards.clear();
+
+		for (final MessageQueue mq : messageQueues) {
+
+			if (null != queueMatcher && !queueMatcher.trigger(mq)) {
+				continue;
+			}
+
+			long maxOffset;
+			long consumedOffset;
+			try {
+				maxOffset = maxOffset(mq);
+				consumedOffset = fetchConsumeOffset(mq, false);
+			} catch (MQClientException e3) {
+				throw new RuntimeException(e3);
+			}
+
+			matchQueue.add(mq);
+			dashboards.put(mq, new ControlStruct(
+					consumedOffset,
+					maxOffset,
+					() -> queueConsume(mq)
+				)
+			);
+		}
+		
+		if(0 == matchQueue.size())
+			throw new RuntimeException("No queue was selected!!!");
+		
+	}
+	
+	private void queueConsume(final MessageQueue mq) {
+
+		/// 语法上无法从lambda中获取到内部类的成员变量或内部类的this指针
+		/// 只能从运行时中获取
+		ControlStruct controlStruct = dashboards.get(mq);
+		
+		long currentOffset = controlStruct.offsetFetchLocal.get();
+		
+		// TODO tag 置为 null，消费端让mqSelecter发挥作用，tag让其在生产端发挥作用吧
+		PullResult pullResult;
+		try {
+			pullResult = pullBlockIfNotFound(mq, null, currentOffset, 32);
+		} catch (MQClientException | RemotingException | MQBrokerException | InterruptedException e) {
+			throw new RuntimeException(e);
+		}
+				
+		switch (pullResult.getPullStatus()) {
+			case FOUND:
+				List<MessageExt> messageExtList = pullResult.getMsgFoundList();
+				for (int i = 0; i<messageExtList.size(); i++) {
+					if(DefaultMQConsumer.this.flowControlFlag) {
+						// 流控,
+						int frezon = 0;
+						int cAmount = 0;
+						while(flowControlSwitch.trigger(mq, cAmount = consumingAmount.get(), frezon)) {
+							if(flowControlLoggerAccquired.compareAndSet(true, false))
+								logger.error("Flow control protected! Amount of on processing message is {}", cAmount);
+							LockSupport.parkNanos(TimeUnit.MILLISECONDS.toNanos(100l));
+							frezon++;
+						}
+						consumingAmount.getAndIncrement();
+					}
+					final long consumingOffset = currentOffset + i + 1;
+					MessageExt rmqMsg = messageExtList.get(i);
+					final EJokerQueueMessage queueMessage = new EJokerQueueMessage(
+						rmqMsg.getTopic(),
+						rmqMsg.getFlag(),
+						rmqMsg.getBody(),
+						rmqMsg.getTags());
+					sumbiter.trigger(() -> 
+					messageProcessor.trigger(queueMessage, message -> tryMarkCompletion(mq, consumingOffset))
+					)
+					;
+				}
+				controlStruct.offsetFetchLocal.getAndSet(pullResult.getNextBeginOffset());
+				controlStruct.offsetMax.getAndSet(pullResult.getMaxOffset());
+				break;
+			case NO_MATCHED_MSG:
+				logger.debug("[state: NO_MATCHED_MSG, topic: {}, queueId: {}]", mq.getTopic(), mq.getQueueId());
+				break;
+			case NO_NEW_MSG:
+				logger.debug("[state: NO_NEW_MSG, topic: {}, queueId: {}]", mq.getTopic(), mq.getQueueId());
+				break;
+			case OFFSET_ILLEGAL:
+				logger.debug("[state: OFFSET_ILLEGAL, topic: {}, queueId: {}]", mq.getTopic(), mq.getQueueId());
+				throw new RuntimeException("OFFSET_ILLEGAL");
+			default:
+				assert false;
+		}
+	}
+	
+	private void tryMarkCompletion(MessageQueue mq, long comsumedOffset) {
+		ControlStruct controlStruct = dashboards.get(mq);
+		if(null != controlStruct.aheadCompletion.putIfAbsent(comsumedOffset, ""))
+			throw new RuntimeException();
+		logger.debug("Receive local completion. Queue: {}, offset {}", mq, comsumedOffset);
+		
+		enableSequenceProcessing();
 	}
 	
 	private void enableSequenceProcessing() {
@@ -374,6 +413,8 @@ public class DefaultMQConsumer extends org.apache.rocketmq.client.consumer.Defau
 
 		public final AtomicLong offsetFetchLocal;
 		
+		public final AtomicLong offsetMax;
+		
 		public final AtomicBoolean offsetDirty;
 		
 		public final IVoidFunction messageHandlingJob;
@@ -382,39 +423,13 @@ public class DefaultMQConsumer extends org.apache.rocketmq.client.consumer.Defau
 		
 		public Thread workThread = null;
 		
-		public ControlStruct(long initOffset, IVoidFunction messageHandlingJob) {
+		public ControlStruct(long initOffset, long maxOffsetCurrent,IVoidFunction messageHandlingJob) {
 			this.offsetConsumedLocal = new AtomicLong(initOffset);
 			this.offsetFetchLocal = new AtomicLong(initOffset);
 			this.messageHandlingJob = messageHandlingJob;
 			this.offsetDirty = new AtomicBoolean(false);
+			this.offsetMax = new AtomicLong(maxOffsetCurrent);
 		}
 		
-	}
-	
-	/**
-	 * 默认异步策略
-	 */
-	private IVoidFunction1<IVoidFunction> sumbiter = c -> new Thread(c::trigger).start();
-	
-	public void useSubmiter(IVoidFunction1<IVoidFunction> sumbiter) {
-		this.sumbiter = sumbiter;
-	}
-	
-	public void configureFlowControl(boolean flag) {
-		this.flowControlFlag = flag;
-	}
-
-	/**
-	 * 1 返回值boolean是否要求流控 true:是 false:不是
-	 * 参数1 当前队列
-	 * 参数2 在处理中消息数
-	 * 参数3 队列检查序数
-	 */
-	public void useFlowControlSwitch(IFunction3<Boolean, MessageQueue, Integer, Integer> sw) {
-		this.flowControlSwitch = sw;
-	}
-	
-	public void useQueueSelector(IFunction1<Boolean, MessageQueue> queueMatcher) {
-		this.queueMatcher = queueMatcher;
 	}
 }
